a2d2618f5ab444bce0fc05cb13648c402a4eaccd
Muhtasim Ferdous <mferdous98@aol.com>
2020-07-03 02:12:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |item, index|
-    array[item] = index
-    list_dwarves
+    array[item] = index.to_s
+    array
 end
 end
